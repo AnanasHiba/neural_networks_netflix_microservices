9487d6ca67c537699e3320d4a8c8af3bdeea7c61
ananas <lynnyk_vova@ukr.net>
2020-05-02 23:27:05 +0300

    basic configuration data-service

--- data-service/src/main/kotlin/com/kpi/dataservice/DataServiceApplication.kt
+++ data-service/src/main/kotlin/com/kpi/dataservice/DataServiceApplication.kt
@@ -2,8 +2,10 @@ package com.kpi.dataservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class DataServiceApplication
 
 fun main(args: Array<String>) {
