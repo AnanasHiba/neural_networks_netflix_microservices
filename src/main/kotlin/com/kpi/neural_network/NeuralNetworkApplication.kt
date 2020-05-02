42c3fbe35d1544aa334d1edd10a6e65672495c39
ananas <lynnyk_vova@ukr.net>
2020-05-02 19:38:19 +0300

    start

--- src/main/kotlin/com/kpi/neural_network/NeuralNetworkApplication.kt
+++ src/main/kotlin/com/kpi/neural_network/NeuralNetworkApplication.kt
@@ -1,9 +1,10 @@
 package com.kpi.neural_network
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class NeuralNetworkApplication
 
 fun main(args: Array<String>) {
