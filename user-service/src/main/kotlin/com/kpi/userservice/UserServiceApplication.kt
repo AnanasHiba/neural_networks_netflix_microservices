c4bc93bb0bffe3c371da3025bcfbef86c8530c88
ananas <lynnyk_vova@ukr.net>
2020-05-03 01:29:41 +0300

    basic configuration user-service

--- user-service/src/main/kotlin/com/kpi/userservice/UserServiceApplication.kt
+++ user-service/src/main/kotlin/com/kpi/userservice/UserServiceApplication.kt
@@ -1,15 +1,13 @@
-package com.kpi.configservice
+package com.kpi.userservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-class ConfigServiceApplication
+class UserServiceApplication
 
 fun main(args: Array<String>) {
-	runApplication<ConfigServiceApplication>(*args)
+	runApplication<UserServiceApplication>(*args)
 }
