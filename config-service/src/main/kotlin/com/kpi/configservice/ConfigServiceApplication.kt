d2cf177c44b1ee65ec2166f8454ad50abe6bd10d
ananas <lynnyk_vova@ukr.net>
2020-05-02 22:51:37 +0300

    servise structure

---
+++ config-service/src/main/kotlin/com/kpi/configservice/ConfigServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.kpi.configservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConfigServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfigServiceApplication>(*args)
+}
