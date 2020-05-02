054c69fd4adf0e1ef42e3a23f60b894df8777dbc
AnanasHiba <lynnyk_vova@ukr.net>
2020-05-02 19:13:15 +0300

    Add files via upload

---
+++ src/main/kotlin/com/kpi/neural_network/NeuralNetworkApplication.kt
@@ -0,0 +1,11 @@
+package com.kpi.neural_network
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NeuralNetworkApplication
+
+fun main(args: Array<String>) {
+	runApplication<NeuralNetworkApplication>(*args)
+}
---
+++ src/test/kotlin/com/kpi/neural_network/NeuralNetworkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kpi.neural_network
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NeuralNetworkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
