233da1be39d12c9d62a93acc4def15c12bb94610
ananas <lynnyk_vova@ukr.net>
2020-05-05 22:13:48 +0300

    auth-service added

--- security-service/src/main/kotlin/com/kpi/securityservice/SecurityServiceApplication.kt
+++ security-service/src/main/kotlin/com/kpi/securityservice/SecurityServiceApplication.kt
@@ -2,8 +2,10 @@ package com.kpi.securityservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class SecurityServiceApplication
 
 fun main(args: Array<String>) {
